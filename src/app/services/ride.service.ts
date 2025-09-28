a6bfb940dd8b521b62516e39a294f26033a250b9
amiticot <icotbpl@gmail.com>
2025-09-28 11:44:29 +0530

    Added demo ride data

--- src/app/services/ride.service.ts
+++ src/app/services/ride.service.ts
@@ -1,22 +1,88 @@
 
 import { Injectable } from '@angular/core';
 import { Ride } from '../models/ride.model';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
 })
 export class RideService {
 
-    private rides: Ride[] = [];
+    private rides: Ride[] = [
+        {
+            rideId: 'RIDE001',
+            employeeId: 'EMP1001',
+            vehicleType: 'Car',
+            vehicleNo: 'KA01AB1234',
+            vacantSeats: 3,
+            time: new Date('2025-10-01T09:00:00'),
+            pickupPoint: 'Whitefield',
+            destination: 'Electronic City',
+            bookedBy: ['EMP1002', 'EMP1003']
+        },
+        {
+            rideId: 'RIDE002',
+            employeeId: 'EMP1004',
+            vehicleType: 'Bike',
+            vehicleNo: 'KA03CD5678',
+            vacantSeats: 1,
+            time: new Date('2025-10-01T08:30:00'),
+            pickupPoint: 'Indiranagar',
+            destination: 'Koramangala',
+            bookedBy: []
+        },
+        {
+            rideId: 'RIDE003',
+            employeeId: 'EMP1005',
+            vehicleType: 'Car',
+            vehicleNo: 'KA05EF9012',
+            vacantSeats: 2,
+            time: new Date('2025-10-01T10:00:00'),
+            pickupPoint: 'Hebbal',
+            destination: 'MG Road',
+            bookedBy: ['EMP1006']
+        },
+        {
+            rideId: 'RIDE004',
+            employeeId: 'EMP1007',
+            vehicleType: 'Car',
+            vehicleNo: 'KA07GH3456',
+            vacantSeats: 4,
+            time: new Date('2025-10-01T09:15:00'),
+            pickupPoint: 'Marathahalli',
+            destination: 'BTM Layout',
+            bookedBy: ['EMP1008', 'EMP1009', 'EMP1010']
+        },
+        {
+            rideId: 'RIDE005',
+            employeeId: 'EMP1011',
+            vehicleType: 'Bike',
+            vehicleNo: 'KA09IJ7890',
+            vacantSeats: 1,
+            time: new Date('2025-10-01T07:45:00'),
+            pickupPoint: 'Jayanagar',
+            destination: 'HSR Layout',
+            bookedBy: []
+        }
+    ];
+
+    private ridesSubject = new BehaviorSubject<Ride[]>([]); // Create BehaviorSubject
+
+    constructor() {
+        // Initialize the observable with the current rides array
+        this.ridesSubject.next(this.rides);
+    }
+
 
     // Custom function to generate a unique rideId
     generateRideId(): string {
         return `ride-${new Date().getTime()}-${Math.floor(Math.random() * 1000)}`;
     }
 
-    // Function to get all rides
-    getRides(): Ride[] {
-        return [...this.rides];
+
+    // Ensure this method returns an observable
+    getRides() {
+        return this.ridesSubject.asObservable();  // Return observable rides list
     }
 
     // Function to add a new ride
@@ -27,6 +93,7 @@ export class RideService {
             bookedBy: []
         };
         this.rides.push(newRide);
+        this.ridesSubject.next(this.rides);  // Emit the new rides list to subscribers
     }
 
     // Function to book a ride
