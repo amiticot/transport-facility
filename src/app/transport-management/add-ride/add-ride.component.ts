d8f51726865d768e79d7e8a4b611584f34b55eee
amiticot <icotbpl@gmail.com>
2025-09-28 16:19:05 +0530

    Restrict ride time input to current date and time onward

--- src/app/transport-management/add-ride/add-ride.component.ts
+++ src/app/transport-management/add-ride/add-ride.component.ts
@@ -8,6 +8,26 @@ import { RideService } from 'src/app/services/ride.service';
   styleUrls: ['./add-ride.component.css']
 })
 export class AddRideComponent {
+
+  minDateTime: any;
+
+  ngOnInit() {
+    const now = new Date();
+    this.minDateTime = this.formatDateToDatetimeLocal(now);
+  }
+
+  formatDateToDatetimeLocal(date: Date): string {
+    const pad = (n: number) => n.toString().padStart(2, '0');
+
+    const year = date.getFullYear();
+    const month = pad(date.getMonth() + 1); // Months are 0-based
+    const day = pad(date.getDate());
+    const hours = pad(date.getHours());
+    const minutes = pad(date.getMinutes());
+
+    return `${year}-${month}-${day}T${hours}:${minutes}`;
+  }
+
   constructor(private fb: FormBuilder, private rideService: RideService) { }
 
   rideForm = this.fb.group({
